1e3f9c48654cb425456bfaeb7f8a919ef181c26e
JVBack-end <jvictor491@gmail.com>
2024-08-17 11:30:49 -0300

    feat:criacao do dto de entrada

---
+++ src/main/java/com/microservice/email/dtos/EmailRequest.java
@@ -0,0 +1,29 @@
+package com.microservice.email.dtos;
+
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class EmailRequest {
+
+    @NotBlank
+    private String ownerRef;
+    @NotBlank
+    @Email
+    private String emailFrom;
+    @NotBlank
+    @Email
+    private String emailTo;
+    @NotBlank
+    private String subject;
+    @NotBlank
+    private String text;
+}
