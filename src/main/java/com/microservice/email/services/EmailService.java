ba299240aa448dc57b08eabf415b6456b5a7c772
JVBack-end <jvictor491@gmail.com>
2024-08-17 12:57:47 -0300

    feat:aplicacao da regra de negocio

--- src/main/java/com/microservice/email/services/EmailService.java
+++ src/main/java/com/microservice/email/services/EmailService.java
@@ -1,9 +1,18 @@
 package com.microservice.email.services;
 
+import com.microservice.email.dtos.EmailRequest;
+import com.microservice.email.enums.StatusEmail;
 import com.microservice.email.models.EmailModel;
 import com.microservice.email.repositories.EmailRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 public class EmailService {
@@ -11,6 +20,27 @@ public class EmailService {
     @Autowired
     EmailRepository emailRepository;
 
-    public void sendEmail(EmailModel emailModel) {
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    public EmailModel sendEmail(@Valid EmailRequest emailRequest) {
+        EmailModel emailModel = new EmailModel();
+        BeanUtils.copyProperties(emailRequest,emailModel);
+        emailModel.setSendDateEmail(LocalDateTime.now());
+
+        try {
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setFrom(emailModel.getEmailFrom());
+            message.setTo(emailModel.getEmailTo());
+            message.setSubject(emailModel.getSubject());
+            message.setText(emailModel.getText());
+
+            javaMailSender.send(message);
+            emailModel.setStatusEmail(StatusEmail.SENT);
+        }catch (MailException e){
+            emailModel.setStatusEmail(StatusEmail.ERROR);
+        }finally {
+             return emailRepository.save(emailModel);
+        }
     }
 }
